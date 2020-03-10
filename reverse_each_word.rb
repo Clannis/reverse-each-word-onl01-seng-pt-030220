f8e52e539e629870dd55d3c8b757caa5a01c22e4
James Dodds <dodds1990@gmail.com>
2020-03-10 02:46:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,33 +1,33 @@
 require 'pry'
 
-# def reverse_each_word(string)
-#   output_array = []
-#   array = string.split(" ")
-#   array.each do |word|
-#     output_word = ""
-#     output_word_array = []
-#     char_array = word.split("")
-#     char_array.each do |char|
-#       output_word_array.unshift(char)
-#     end
-#     output_word = output_word_array.join
-#     output_array << (output_word)
-#   end
-#   output_array.join(" ")
-# end
-
 def reverse_each_word(string)
-  word_array = string.split(" ")
-  word_array.collect do |word|
-    # example 1
-    word.reverse
-    # example 2
-    #"#{word.reverse}"
-    #binding.pry
+  output_array = []
+  array = string.split(" ")
+  array.each do |word|
+    output_word = ""
+    output_word_array = []
+    char_array = word.split("")
+    char_array.each do |char|
+      output_word_array.unshift(char)
+    end
+    output_word = output_word_array.join
+    output_array << (output_word)
   end
-  puts word_array
-  output = word_array.join(" ")
-  output
+  output_array.join(" ")
 end
 
-reverse_each_word("Hello there, and how are you?")
+# def reverse_each_word(string)
+#   word_array = string.split(" ")
+#   word_array.collect do |word|
+#     # example 1
+#     word.reverse
+#     # example 2
+#     #"#{word.reverse}"
+#     #binding.pry
+#   end
+#   puts word_array
+#   output = word_array.join(" ")
+#   output
+# end
+#
+# reverse_each_word("Hello there, and how are you?")
