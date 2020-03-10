ef84dbc4040bf47f5ac513fc6a29c8db1cade8c2
James Dodds <dodds1990@gmail.com>
2020-03-10 03:01:39 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,27 @@
+# def reverse_each_word(string)
+#   output_array = []
+#   array = string.split(" ")
+#   array.collect do |word|
+#     output_word = ""
+#     output_word_array = []
+#     char_array = word.split("")
+#     char_array.each do |char|
+#       output_word_array.unshift(char)
+#     end
+#     output_word = output_word_array.join
+#     output_array << (output_word)
+#   end
+#   output = output_array.join(" ")
+#   puts output
+#   output
+# end
+# poop = " helle there you"
+# reverse_each_word(poop)
+
+def reverse_each_word(string)
+  word_array = string.split(" ")
+  output = word_array.collect do |word|
+    word.reverse
+  end
+  output.join(" ")
+end
